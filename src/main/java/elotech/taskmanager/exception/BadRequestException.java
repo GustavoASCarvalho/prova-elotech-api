92c4bd1f23504a564e12839626dda26c211fcefe
Gustavo Alexandre da Silva Carvalho <gustavoalexandrescarvalho@gmail.com>
2026-03-15 11:29:01 -0300

    feat: Adiciona exceções personalizadas e manipulador global de exceções

---
+++ src/main/java/elotech/taskmanager/exception/BadRequestException.java
@@ -0,0 +1,8 @@
+package elotech.taskmanager.exception;
+
+public class BadRequestException extends RuntimeException {
+
+    public BadRequestException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/elotech/taskmanager/exception/NotFoundException.java
@@ -0,0 +1,8 @@
+package elotech.taskmanager.exception;
+
+public class NotFoundException extends RuntimeException {
+
+    public NotFoundException(String message) {
+        super(message);
+    }
+}
